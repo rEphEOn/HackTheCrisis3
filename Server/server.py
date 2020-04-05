6ffac1aefc69017fa1097c994b7d9c84b5da822a
Almantas <repheon@yahoo.com>
2020-04-05 11:28:08 +0300

    minimized things a bit

--- Server/server.py
+++ Server/server.py
@@ -24,53 +24,137 @@
 #
 ###############################################################################
 
-import uuid
 import sys
+import uuid
 
-from twisted.python import log
 from twisted.internet import reactor
+from twisted.python import log
 from twisted.web.server import Site
+from twisted.web.static import File
+
 from twisted.web.wsgi import WSGIResource
 
 from flask import Flask, render_template, request
 
 from autobahn.twisted.websocket import WebSocketServerFactory, \
-    WebSocketServerProtocol
+    WebSocketServerProtocol, \
+    listenWS
 
 from autobahn.twisted.resource import WebSocketResource, WSGIRootResource
 
-
-# Our WebSocket Server protocol
-class EchoServerProtocol(WebSocketServerProtocol):
-
-    def onMessage(self, payload, isBinary):
-        self.sendMessage(payload, isBinary)
-
-
-# Our WSGI application .. in this case Flask based
 app = Flask(__name__)
 app.secret_key = str(uuid.uuid4())
+
+class BroadcastServerProtocol(WebSocketServerProtocol):
+
+    def onOpen(self):
+        self.factory.register(self)
+
+    def onMessage(self, payload, isBinary):
+        if not isBinary:
+            msg = "{} from {}".format(payload.decode('utf8'), self.peer)
+            self.factory.broadcast(msg)
+
+    def connectionLost(self, reason):
+        WebSocketServerProtocol.connectionLost(self, reason)
+        self.factory.unregister(self)
+
+
+class BroadcastServerFactory(WebSocketServerFactory):
+
+    """
+    Simple broadcast server broadcasting any message it receives to all
+    currently connected clients.
+    """
+
+    def __init__(self, url):
+        WebSocketServerFactory.__init__(self, url)
+        self.clients = []
+        self.tickcount = 0
+        self.tick()
+
+    def tick(self):
+        self.tickcount += 1
+        # self.broadcast("tick %d from server" % self.tickcount)
+        reactor.callLater(1, self.tick)
+
+    def register(self, client):
+        if client not in self.clients:
+            print("registered client {}".format(client.peer))
+            self.clients.append(client)
+
+    def unregister(self, client):
+        if client in self.clients:
+            print("unregistered client {}".format(client.peer))
+            self.clients.remove(client)
+
+    def broadcast(self, msg):
+        print("broadcasting message '{}' ..".format(msg))
+        for c in self.clients:
+            c.sendMessage(msg.encode('utf8'))
+            print("message sent to {}".format(c.peer))
+
+
+
+
+class BroadcastPreparedServerFactory(BroadcastServerFactory):
+
+    """
+    Functionally same as above, but optimized broadcast using
+    prepareMessage and sendPreparedMessage.
+    """
+
+    def broadcast(self, msg):
+        print("broadcasting prepared message '{}' ..".format(msg))
+        preparedMsg = self.prepareMessage(msg)
+        for c in self.clients:
+            c.sendPreparedMessage(preparedMsg)
+            print("prepared message sent to {}".format(c.peer))
 
 
 @app.route('/')
 def page_home():
     return render_template('index.html')
 
+# @app.route('/yee/<id>', methods=['POST'])
+# def broadcast_wash_of_hands(id):
+#     # content = request.json
+#     print(request.form.get("yee", ""))
+#     msg = f"{id} washed their hands!"
+#     #    return jsonify({"uuid":uuid})
+#     self.broadcast("tick %d from server" % msg)
+# @app.route('/yee/<id>', methods=['POST'])
+# def do_something(id):
+#     # content = request.json
+#     print('some text')
+#     print(request.form.get("yee", ""))
+#     print(id)
+#     #    return jsonify({"uuid":uuid})
+#     return "yeeah!"
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     log.startLogging(sys.stdout)
 
-    # create a Twisted Web resource for our WebSocket server
-    wsFactory = WebSocketServerFactory("ws://127.0.0.1:8080")
-    wsFactory.protocol = EchoServerProtocol
-    wsResource = WebSocketResource(wsFactory)
+    ServerFactory = BroadcastServerFactory
+    # ServerFactory = BroadcastPreparedServerFactory
 
-    # create a Twisted Web WSGI resource for our Flask server
+    wsFactory = ServerFactory("ws://127.0.0.1:9000")
+    wsFactory.protocol = BroadcastServerProtocol
+    listenWS(wsFactory)
+    wsResource = WebSocketResource(wsFactory)
     wsgiResource = WSGIResource(reactor, reactor.getThreadPool(), app)
 
+    # routing into broadcast
+    @app.route('/yee/<id>', methods=['POST'])
+    def broadcast_wash_of_hands(id):
+        # content = request.json
+        # print(request.form.get("yee", ""))
+        msg = f"{id} washed their hands!"
+        #    return jsonify({"uuid":uuid})
+        wsFactory.broadcast(msg)
+        return f"Thanks for washing the hands, {id}\n"
     # create a root resource serving everything via WSGI/Flask, but
     # the path "/ws" served by our WebSocket stuff
     rootResource = WSGIRootResource(wsgiResource, {b'ws': wsResource})
@@ -78,12 +162,8 @@ if __name__ == "__main__":
     # create a Twisted Web Site and run everything
     site = Site(rootResource)
 
-    @app.route('/yee', methods=['GET', 'POST'])
-    def do_something():
-        # content = request.json
-        print('some text')
-        #    return jsonify({"uuid":uuid})
-        return "yeeah!"
-
+    webdir = File(".")
+    web = Site(webdir)
     reactor.listenTCP(8080, site)
+
     reactor.run()
